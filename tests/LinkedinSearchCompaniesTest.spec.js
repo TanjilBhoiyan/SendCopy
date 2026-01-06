21fdf5f6bf2c187adad4c7bae7f734a219183ea7
TanjilBhoiyan <tanjirbhuiyan@gmail.com>
2026-01-06 12:13:03 +0600

    Add negative test cases for linkedin search companies

--- tests/LinkedinSearchCompaniesTest.spec.js
+++ tests/LinkedinSearchCompaniesTest.spec.js
@@ -3,19 +3,14 @@ import { LoginPage } from '../pages/LoginPage';
 import { LinkedinLeadsPage } from '../pages/LinkedinLeadsPage';
 import { GenerateLeadsPage } from '../pages/GenerateLeadsPage';
 import { LinkedInSearchBarPage } from '../pages/LinkedInSearchBarPage';
-//import { LinkedinEventAttendeesPage} from '../pages/LinkedInEventAttendeesPage';
 import { LinkedinSearchCompanies } from '../pages/LinkedInSearchCompaniesPage';
-//import { ImprtFromCSVPage } from '../pages/ImportFromCSVPage';
+
 
 test.describe.configure({mode:'serial'});
 
 test.describe('Linkedin Leads Test',()=>{
-    /**@type {ImprtFromCSVPage} */
-    let imprtfromcsvpage;
     /**@type {LinkedinSearchCompanies} */
     let linkedinsearchcompanies;
-    /**@type {LinkedinEventAttendeesPage} */
-    let linkedineventattendees;
     /** @type {LinkedinLeadsPage} */
     let linkedinleads;
     /**@type {GenerateLeadsPage} */
@@ -29,12 +24,43 @@ test.describe('Linkedin Leads Test',()=>{
         linkedinleads = new LinkedinLeadsPage(page);
         await linkedinleads.linkedinLeadsLink();
 
-
         // create object for generate leads page
         generateLeads = new GenerateLeadsPage(page);
         linkedinsearchbar = new LinkedInSearchBarPage(page);
-        linkedineventattendees = new LinkedinEventAttendeesPage(page);
         linkedinsearchcompanies = new LinkedinSearchCompanies(page);
-        imprtfromcsvpage = new ImprtFromCSVPage(page);
+    })
+
+    test('Verify withour inter any list name ',async ({page})=>{
+        await linkedinleads.addLeadsButton();
+        await generateLeads.linkedinSearchCompanies().click();
+        await generateLeads.continueButton().click();
+        //await linkedinsearchbar.listNameInputField('Companies lead 1');
+        await linkedinsearchbar.selectSenderName('Shakil Bhuiyan');
+        await linkedinsearchcompanies.searchQueryInput('Chocolate');
+        await page.waitForTimeout(2000);
+        await linkedinsearchbar.startImportButton().click();
+        await expect(page.getByText('List name is required')).toBeVisible();       
+    })
+    test('Verify without selecting any sender name',async ({page})=>{
+        await linkedinleads.addLeadsButton();
+        await generateLeads.linkedinSearchCompanies().click();
+        await generateLeads.continueButton().click();
+        await linkedinsearchbar.listNameInputField('Companies lead 1');
+        await linkedinsearchbar.selectSenderName('Shakil Bhuiyan');
+        await linkedinsearchcompanies.searchQueryInput('Chocolate');
+        await page.waitForTimeout(2000);
+        await linkedinsearchbar.startImportButton().click();
+        await expect(page.getByText('At least one sender must be selected')).toBeVisible();       
+    })
+    test('Verify not inter any keyword in search query field',async ({page})=>{
+        await linkedinleads.addLeadsButton();
+        await generateLeads.linkedinSearchCompanies().click();
+        await generateLeads.continueButton().click();
+        await linkedinsearchbar.listNameInputField('Companies lead 1');
+        await linkedinsearchbar.selectSenderName('Shakil Bhuiyan');
+        await linkedinsearchcompanies.searchQueryInput('');
+        await page.waitForTimeout(2000);
+        await linkedinsearchbar.startImportButton().click();
+        await expect(page.getByText('Keyword is required')).toBeVisible();       
     })
 })
