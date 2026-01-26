b7c4f79786c7d253684b20fe8162ed8ebdfcf0a2
TanjilBhoiyan <tanjirbhuiyan@gmail.com>
2026-01-26 22:11:14 +0600

    add configure limit slider test cases

--- pages/LinkedInAccountsPages/LinkedinAccountPage.js
+++ pages/LinkedInAccountsPages/LinkedinAccountPage.js
@@ -16,8 +16,101 @@ export class LinkedinAccountPage{
         this.connectCookiesButtonLocator='//button[normalize-space()="Connect using cookies"]';
         this.nextStepButtonLocator='//button[normalize-space()="Next Step"]';
         this.liatValueInputLocator='textarea[placeholder="Paste your LIAT value here..."]';
-        //this.connectAccountButtonLocator='//button[normalize-space()="Connect Account"]'
         this.previousButtonLocator='//button[normalize-space()="Previous"]';
+        this.verifyYourProxyDivLocator='//div[@class="space-y-2"]';
+        this.configureLimitButtonLocator='(//button[normalize-space()="Configure limits"])[1]';
+        this.updateSettingsButtonLocator='//button[normalize-space()="Update Settings"]';
+        this.maxFollowDaySliderLocator='#max-follows-day'
+        this.maxFollowDayValueLocator='//body[1]/div[4]/div[3]/form[1]/div[2]/div[1]/div[1]/div[1]';
+        this.maxMessageDaySliderLocator='#max-msg-day';
+        this.maxMessageDayValueLocator='//body[1]/div[4]/div[3]/form[1]/div[3]/div[1]/div[1]/div[1]';
+        this.maxInmailMessageDaySliderLocator='#max-in-mail-msg-day'
+        this.maxInmailMessageDayValueLocator='//body[1]/div[4]/div[3]/form[1]/div[4]/div[1]/div[1]/div[1]'
+        this.maxConnectionRequestDaySliderLocator='#max-connection-requests-day';
+        this.maxConnectionRequestDayValueLocator='//body[1]/div[4]/div[3]/form[1]/div[5]/div[1]/div[1]/div[1]';
+        this.maxProfileViewDaySliderLocator='#max-profile-views-day';
+        this.maxProfileViewDayValueLocator='//body[1]/div[4]/div[3]/form[1]/div[6]/div[1]/div[1]/div[1]';
+        this.maxPostLikesDaySliderLocator='#max-like-posts-per-day';
+        this.maxPostLikesDayValueLocator='//body[1]/div[4]/div[3]/form[1]/div[7]/div[1]/div[1]/div[1]';
+    }
+    async maxPostLikesDay(set_value){
+        await this.sliderMove(this.maxPostLikesDaySliderLocator, this.maxConnectionRequestDayValueLocator,set_value);
+    }
+    async maxProfileViewsDay(set_value){
+        await this.sliderMove(this.maxProfileViewDaySliderLocator, this.maxConnectionRequestDayValueLocator,set_value);
+    }
+    async maxConnectionRequestDay(set_value){
+        await this.sliderMove(this.maxConnectionRequestDaySliderLocator, this.maxConnectionRequestDayValueLocator,set_value);
+    }
+    async inMailMessageDay(set_value){
+        await this.sliderMove(this.maxInmailMessageDaySliderLocator, this.maxInmailMessageDayValueLocator,set_value);
+    }
+    async maxMessageDay(set_value){
+        await this.sliderMove(this.maxMessageDaySliderLocator, this.maxConnectionRequestDayValueLocator,set_value);
+    }
+    async maxFollowDay(set_value){
+        await this.sliderMove(this.maxFollowDaySliderLocator,this.maxFollowDayValueLocator,set_value);
+    }
+
+    async sliderMove(slider_bar,slider_value,set_value){
+        await this.page.waitForSelector(slider_bar);
+        
+        const sliderHandle = await this.page.$(slider_bar);
+        const valueDisplay = this.page.locator(slider_value);
+
+        let targetAmount = set_value; // target value
+        
+        if (sliderHandle) {
+            const srcBound = await sliderHandle.boundingBox();
+            if (srcBound) {
+                // finding the current value
+                let currentText = await valueDisplay.textContent();
+                let currentValue = parseInt(currentText.trim());
+                console.log('Starting from: ' + currentValue);
+
+                // slider er current position 
+                let startX = srcBound.x + (srcBound.width * (currentValue / 100));
+                let currentY = srcBound.y + srcBound.height / 2;
+
+                await this.page.mouse.move(startX, currentY);
+                await this.page.mouse.down();
+
+                let isCompleted = false;
+                let moveX = startX;
+
+                while (!isCompleted) {
+                    // if current value less than target value then move right 
+                    if (currentValue < targetAmount) {
+                        moveX += 2;
+                    } 
+                    // if current value already equal or grater target value
+                    else {
+                        isCompleted = true;
+                        break;
+                    }
+                    await this.page.mouse.move(moveX, currentY);
+                    // check the new updated value
+                    let updatedText = await valueDisplay.textContent();
+                    currentValue = parseInt(updatedText.trim());
+
+                    if (currentValue >= targetAmount) {
+                        isCompleted = true;
+                    }
+                }
+                await this.page.mouse.up();
+                console.log('Final Value Reached: ' + currentValue);
+            }
+        }
+    }
+
+    updateSettingsButton(){
+        return this.page.locator(this.updateSettingsButtonLocator);
+    }
+    async configureLimitButton(){
+        await this.page.locator(this.configureLimitButtonLocator).click();
+    }
+    verifyYourProxyDiv(){
+        return this.page.locator(this.verifyYourProxyDivLocator);
     }
     async previousButton(){
         await this.page.locator(this.previousButtonLocator).click();
--- tests/LinkedinAccountsTests/LinkedinAccountsTest.spec.js
+++ tests/LinkedinAccountsTests/LinkedinAccountsTest.spec.js
@@ -1,6 +1,7 @@
 import { test , expect } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 import { LinkedinAccountPage } from '../../pages/LinkedInAccountsPages/LinkedinAccountPage';
+import testData from '../../testData/testData.json';
 
 
 test.describe.configure({mode:'serial'});
@@ -17,11 +18,12 @@ test.describe('Linkedin Leads Test',()=>{
 
         // create object for generate leads page
     })
-    // test.only('',async ({page})=>{
-        
-
-    // })
-    test.only('After click on previous button check page will go to previous page ',async ({page})=>{
+    test('If select a proxy required country should appear verify your proxy option',async ({page})=>{
+        await linkedinAccount.connectAccountButton().click();
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.country);
+        await expect(linkedinAccount.verifyYourProxyDiv()).toHaveCount(1);
+    })
+    test('After click on previous button check page will go to previous page ',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
         await linkedinAccount.connectCookiesButton();
         await linkedinAccount.nextStepButton();
@@ -32,7 +34,7 @@ test.describe('Linkedin Leads Test',()=>{
         await linkedinAccount.connectAccountButton().click();
         await linkedinAccount.connectCookiesButton();
         await linkedinAccount.nextStepButton();
-        await linkedinAccount.liatValueInput('AQEFAa0BAAAAABngAyYAAAGanOZMMkVCQ3RLaW9yQ2FLbFpXWk1ZUVF4S2tveWVNQ00yQk1yeXBhdHg1U1JBdGJmS3doUkhFcUtqeUZBUXpZcS9ZcURBd0FnQzF0Z2Y2XnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjM0Mjk1MTc2NCw2MjIxNDA2OTcpXnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjM0Mzk3NjM4MCw2MjY5NzAzOTMpXnVybjpsaTptZW1iZXI6MTA2OTY4OTE0MLWCKi7W0TEX7HwofEBZaV0_Kr0KHZmgXpGNy9zKYpDYRPj4spGoxoglr15VmKV15PvbrtPJMGzM_gCwIY_hG8JojoHKC7IBzze9pwSfREQL5Yn')
+        await linkedinAccount.liatValueInput(testData.linkedinAccountSPages.invalid_LIAT_token);
         await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
@@ -40,25 +42,25 @@ test.describe('Linkedin Leads Test',()=>{
         await linkedinAccount.connectAccountButton().click();
         await linkedinAccount.connectCookiesButton();
         await linkedinAccount.nextStepButton();
-        await linkedinAccount.liatValueInput('AQEFAa0BAAAAABngAyYAAAGanOZMMkVCQ3RLaW9yQ2FLbFpXWk1ZUVF4S2tveWVNQ00yQk1yeXBhdHg1U1JBdGJmS3doUkhFcUtqeUZBUXpZcS9ZcURBd0FnQzF0Z2Y2XnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjM0Mjk1MTc2NCw2MjIxNDA2OTcpXnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjM0Mzk3NjM4MCw2MjY5NzAzOTMpXnVybjpsaTptZW1iZXI6MTA2OTY4OTE0MLWCKi7W0TEX7HwofEBZaV0_Kr0KHZmgXpGNy9zKYpDYRPj4spGoxoglr15VmKV15PvbrtPJMGzM_gCwIY_hG8JojoHKC7IBzze9pwSfREQL5Yn')
-        await linkedinAccount.selectCountry('BD - Bangladesh');
+        await linkedinAccount.liatValueInput(testData.linkedinAccountSPages.invalid_LIAT_token)
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.country);
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
     test('Try to connect linkedin account using cookies connect without LIAT value',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
         await linkedinAccount.connectCookiesButton();
         await linkedinAccount.nextStepButton();
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('BD - Bangladesh');
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.country);
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
     test('Try to connect linkedin account using cookies connect and using invalid cookies LIAT value',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
         await linkedinAccount.connectCookiesButton();
         await linkedinAccount.nextStepButton();
-        await linkedinAccount.liatValueInput('AQEFAa0BAAAAABngAyYAAAGanOZMMkVCQ3RLaW9yQ2FLbFpXWk1ZUVF4S2tveWVNQ00yQk1yeXBhdHg1U1JBdGJmS3doUkhFcUtqeUZBUXpZcS9ZcURBd0FnQzF0Z2Y2XnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjM0Mjk1MTc2NCw2MjIxNDA2OTcpXnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjM0Mzk3NjM4MCw2MjY5NzAzOTMpXnVybjpsaTptZW1iZXI6MTA2OTY4OTE0MLWCKi7W0TEX7HwofEBZaV0_Kr0KHZmgXpGNy9zKYpDYRPj4spGoxoglr15VmKV15PvbrtPJMGzM_gCwIY_hG8JojoHKC7IBzze9pwSfREQL5Yn')
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('BD - Bangladesh');
+        await linkedinAccount.liatValueInput(testData.linkedinAccountSPages.invalid_LIAT_token);
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.country);
         await linkedinAccount.connectAccount().click();
         await expect(page.getByText('invalid credential')).toBeVisible();
     })
@@ -71,66 +73,66 @@ test.describe('Linkedin Leads Test',()=>{
 // connect with credential
     test('Try to connect using valid email and password',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('xyzbhoiyan@gmail.com');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.wrong_linkedin_email);
         await linkedinAccount.linkedinPasswordInput('Tanjil123');
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('AR - Argentina');
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.without_proxy_country);
         await linkedinAccount.connectAccount().click();
         await expect(page.getByText('Login Successfull')).toBeVisible();
     })
     test('Try to connect using wrong linkedin email',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('xyzbhoiyan@gmail.com');
-        await linkedinAccount.linkedinPasswordInput('Tanjil123');
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('AR - Argentina');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.wrong_linkedin_email);
+        await linkedinAccount.linkedinPasswordInput(testData.linkedinAccountSPages.valid_linkedin_email);
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.without_proxy_country);
         await linkedinAccount.connectAccount().click();
         await expect(page.getByText('Invalid credentials')).toBeVisible();
     })
     test('Try to connect linkedin without select your country',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('tanjilbhoiyan@gmail.com');
-        await linkedinAccount.linkedinPasswordInput('Tanjil123');
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.valid_linkedin_email);
+        await linkedinAccount.linkedinPasswordInput(testData.linkedinAccountSPages.valid_linkedin_email);
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
         //await linkedinAccount.selectCountry('AR - Argentina');
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
     test('Try to connect linkedin without selecting conversation to load into',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('tanjilbhoiyan@gmail.com');
-        await linkedinAccount.linkedinPasswordInput('Tanjil123');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.valid_linkedin_email);
+        await linkedinAccount.linkedinPasswordInput(testData.linkedinAccountSPages.valid_linkedin_email);
         //await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('AR - Argentina');
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.without_proxy_country);
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
     test('Try to connect linkedin using less than 6 character password',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('tanjilbhoiyan@gmail.com');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.valid_linkedin_email);
         await linkedinAccount.linkedinPasswordInput('Tanji');
         await expect(page.getByText('Password must be at least 6 characters')).toBeVisible();
     })
     test('Try to connect linkedin without password',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('tanjilbhoiyan@gmail.com');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.valid_linkedin_email);
         await linkedinAccount.linkedinPasswordInput('');
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('AR - Argentina');
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.without_proxy_country);
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
     test('Try to connect linkedin invalid email address',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
-        await linkedinAccount.linkedinEmailAddressInput('tanjilbhoiyan.com');
-        await linkedinAccount.linkedinPasswordInput('Tanjil123@#?');
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('AR - Argentina');
+        await linkedinAccount.linkedinEmailAddressInput(testData.linkedinAccountSPages.invalid_email);
+        await linkedinAccount.linkedinPasswordInput(testData.linkedinAccountSPages.linkedin_account_password);
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.without_proxy_country);
         await expect(page.getByText('Invalid email address')).toBeVisible();
     })
     test('Try to connect linkedin without email address',async ({page})=>{
         await linkedinAccount.connectAccountButton().click();
         await linkedinAccount.linkedinEmailAddressInput('');
-        await linkedinAccount.linkedinPasswordInput('Tanjil123@#?');
-        await linkedinAccount.inboxPrivacyConfiguration('Track and import all LinkedIn conversations');
-        await linkedinAccount.selectCountry('AR - Argentina');
+        await linkedinAccount.linkedinPasswordInput(testData.linkedinAccountSPages.linkedin_account_password);
+        await linkedinAccount.inboxPrivacyConfiguration(testData.linkedinAccountSPages.select_conversation);
+        await linkedinAccount.selectCountry(testData.linkedinAccountSPages.without_proxy_country);
         await expect(linkedinAccount.connectAccount()).toBeDisabled();
     })
     test('Check the connect account modal opens properly',async ({page})=>{
@@ -141,6 +143,81 @@ test.describe('Linkedin Leads Test',()=>{
         await linkedinAccount.purchaseSeatsButton().click();
         await expect(page).toHaveURL('https://app.sendcopy.ai/settings/billing');
     })
+    test('Try to set max follows/Day grater than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxFollowDay('41');
+        await expect(page.getByText('Value must be at most 40')).toBeVisible();
+        //await linkedinAccount.updateSettingsButton().click();
+    });
+    test.only('Try to set max follows/Day less than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxFollowDay('39');
+        await linkedinAccount.updateSettingsButton().click();
+        await expect(page.getByText('Account limits updated successfully')).toBeVisible();
+    });
+    test('Try to set max message/Day grater than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxMessageDay('41');
+        await expect(page.getByText('Value must be at most 40')).toBeVisible();
+        //await linkedinAccount.updateSettingsButton().click();
+    });
+    test('Try to set max message/Day less than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxMessageDay('39');
+        await linkedinAccount.updateSettingsButton().click();
+        await expect(page.getByText('Account limits updated successfully')).toBeVisible();
+    });
+
+    test('Try to set max InMail/Day grater than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.inMailMessageDay('41');
+        await expect(page.getByText('Value must be at most 40')).toBeVisible();
+        //await linkedinAccount.updateSettingsButton().click();
+    });
+    test.only('Try to set max InMail/Day less than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.inMailMessageDay('39');
+        await linkedinAccount.updateSettingsButton().click();
+        await expect(page.getByText('Account limits updated successfully')).toBeVisible();
+    });
+    test('Try to set max Connection Request/Day grater than 25', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxConnectionRequestDay('26');
+        await expect(page.getByText('Value must be at most 40')).toBeVisible();
+        //await linkedinAccount.updateSettingsButton().click();
+    });
+    test.only('Try to set max Connection Request/Day less than 25', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxConnectionRequestDay('24');
+        await linkedinAccount.updateSettingsButton().click();
+        await expect(page.getByText('Account limits updated successfully')).toBeVisible();
+    });
+    test('Try to set max Profile Views/Day grater than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxProfileViewsDay('41');
+        await expect(page.getByText('Value must be at most 40')).toBeVisible();
+        //await linkedinAccount.updateSettingsButton().click();
+    });
+    test.only('Try to set max Profile Views/Day less than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxProfileViewsDay('39');
+        await linkedinAccount.updateSettingsButton().click();
+        await expect(page.getByText('Account limits updated successfully')).toBeVisible();
+    });
+
+    test('Try to set max Post Like/Day grater than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxPostLikesDay('41');
+        await expect(page.getByText('Value must be at most 40')).toBeVisible();
+        //await linkedinAccount.updateSettingsButton().click();
+    });
+    test.only('Try to set max Post Like/Day less than 40', async ({ page }) => {
+        await linkedinAccount.configureLimitButton();
+        await linkedinAccount.maxPostLikesDay('39');
+        await linkedinAccount.updateSettingsButton().click();
+        await expect(page.getByText('Account limits updated successfully')).toBeVisible();
+    });
+
 })
 
 // npx playwright test tests/LinkedinAccountsTests/LinkedinAccountsTest.spec.js --project chromium --debug
